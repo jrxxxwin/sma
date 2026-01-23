a0239c1e7bd1f85332f856ec1567a75821f78f92
jrxxxwin <winjerombajen@gmail.com>
2026-01-23 18:23:06 +0800

    make changes in logiic between OT and regHOURS

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 let datas = [];
-let pastMonthDatas = [];
+let pastMonthDatas = [
+
+];
+
+const monthsFixed = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 window.onload = function() {
     renderTable();
     getDate()   
+    renderLastMonth()
 }
 const today = new Date();
 const day = today.getDate();
@@ -12,9 +17,7 @@ function printPage() {
     window.print();
 }
 function getDate() {
-    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    
-    let month = months[today.getMonth()];
+    let month = monthsFixed[today.getMonth()];
     const conta = document.getElementById('monthContainer');
     conta.innerText = month;
     
@@ -55,7 +58,6 @@ function addEntry() {
 
     if (!!dateObj.valueOf()) {
         const days = dateObj.getDate();
-
         
         if (day <= 15) {
         if (days <= 15) {
@@ -76,8 +78,6 @@ function addEntry() {
             pastMonthDatas.push(datas)
         } 
     }
-
-    localStorage.setItem("months", JSON.stringify(pastMonthDatas));
     localStorage.setItem("datas", jsonaray);
     inpuits.forEach(input => input.value = '');
     window.location.reload();
@@ -95,48 +95,49 @@ function renderTable() {
     let salarytotal = 0;
 
     datas.forEach((data, index) => {
-
+        let {date, timeIN, timeOUT, hrs} = data;
+        // let month = monthsFixed[date.split('-')[1]];
         datas.sort((a, b) => new Date(a.date) - new Date(b.date));
 
         let hrsworked
         let hrsworkedFixed
-        if (data.timeOUT < data.timeIN) {
-            hrsworked = (data.timeOUT - 0) + (24 - data.timeIN) ;
-            hrsworkedFixed = hrsworked.toFixed(2);
+        if (timeOUT < timeIN) {
+            hrsworked = (timeOUT - 0) + (24 - timeIN) ;
+            hrsworkedFixed = parseInt(hrsworked.toFixed(2));
         }
-        else if (data.timeOUT > data.timeIN) {
-            hrsworked = data.timeOUT - data.timeIN;
-            hrsworkedFixed = hrsworked.toFixed(2);
+        else if (timeOUT > timeIN) {
+            hrsworked = timeOUT - timeIN;
+            hrsworkedFixed = parseInt(hrsworked.toFixed(2));
         }
         
     
         let OT = data.hrs - hrsworked;
-
+        let OTF = OT;
         if (OT < 0) {
-            OT = OT * -1;
+            OTF = OT * -1;
         }
-        if (data.hrs >= hrsworked) {
-            OT = 0;
+        if (hrs >= hrsworked) {
+            OTF = 0;
         }
-        let amount = (hrsworked + OT) * 71.35
-        let amountFixed = amount.toFixed(2);
+        let amount = (hrsworked) * 71.35
+        let amountFixed = parseInt(amount.toFixed(2));
 
+        pastMonthDatas.push(month = {date, timeIN, timeOUT, hrsworkedFixed, hrs, OTF, amountFixed});
         const row = `<tr>
             <td>${data.date}</td>
             <td>${data.timeIN.toFixed(2)}</td>
             <td>${data.timeOUT.toFixed(2)}</td>
             <td>${hrsworkedFixed}</td>
             <td>${data.hrs}</td>
-            <td>${OT.toFixed(2)}</td>
+            <td>${OTF.toFixed(2)}</td>
             <td>₱${amountFixed}</td>
             <td><button onclick="deleteEntrey(${index})">Delete</button></td>
         </tr>`;
         htmlsss += row;
-        salarytotal += parseFloat(amountFixed);
+        salarytotal += (amountFixed);
     })
     tableBody.innerHTML = htmlsss;
-    totalamount.innerText = "₱" +salarytotal.toFixed(2);
-    console.log(datas);
+    totalamount.innerText = "₱" +salarytotal;
 }
 function deleteEntrey(index) {
     datas.splice(index, 1);
@@ -144,3 +145,38 @@ function deleteEntrey(index) {
     localStorage.setItem("datas", jsonaray);
     window.location.reload();
 }
+
+// function renderLastMonth() {
+//     const monthContainer = document.getElementById('pastMonthTable');
+
+//     let html = ''
+
+//     pastMonthDatas.forEach((monthData, monthIndex) => {
+//         let monthName = monthsFixed[monthIndex];
+//         let year = pastMonthDatas[monthIndex][0]?.date.split('-')[0];
+//         let totalHrs = 0;
+//         let totalOT = 0;
+//         let totalRegular = 0;
+//         let totalAmount = 0;
+
+//         // monthData.forEach(entry => {
+//         //     // totalHrs += ;
+//         //     totalOT += entry.hrs - (entry.timeOUT < entry.timeIN ? (entry.timeOUT + 24 - entry.timeIN) : (entry.timeOUT - entry.timeIN));
+//         //     totalRegular += entry.hrs
+//         // })
+
+//         const row = `
+//             <tr>
+//                 <td>${monthName} ${year}</td>
+//                 <td>${totalHrs}</td>
+//                 <td>${totalRegular}</td>
+//                 <td>${totalOT}</td>
+//                 <td>$1,150</td>
+//                 <td>$1,000 ($25/hr) + $150 ($50/hr)</td>
+//             </tr>
+//         `
+//         html += row;
+//     })
+//     monthContainer.innerHTML = html;
+//     console.log(html);
+// }
